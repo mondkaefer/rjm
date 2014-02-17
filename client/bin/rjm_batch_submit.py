ec12df9e7cb525a4c5303f3921ce68557eb509e9
Martin Feller <martinfeller73@gmail.com>
2014-02-17 13:34:48 +1300

    write job config file in various states. use config dict

--- client/bin/rjm_batch_submit.py
+++ client/bin/rjm_batch_submit.py
@@ -1,24 +1,31 @@
 import os
 import sys
 import argparse
-import ConfigParser
+import traceback
 import cer.client.ssh as ssh
 import cer.client.job as job
 import cer.client.util as util
+import cer.client.util.config as config
 from cer.client.util import Retry
 
-# name of the file that contains the list of input files to be uploaded for a job
-infiles_file = 'gridfiles_in.txt'
-ssh_conn = None
+def cleanup():
+  ''' close ssh connection. '''
+  if ssh_conn:
+    try:
+      ssh.close_connection(ssh_conn)
+    except:
+      pass
 
 # information displayed as help by argparse
 h = {
-  'account': 
-    'account code this job is run under, e.g. uoa00042',
+  'account':
+    'Account code this job is run under, e.g. uoa00042, ' +
+    'If not account is specified, the default account as specified in %s is used.' % config.get_config_file(),
   'cmd': 
     'Command to run.',
   'remotebasedir':
-    'Remote base directory where the individual job directories for each job will be created.',
+    'Remote base directory where the individual job directories for each job will be created. ' +
+    'If no remote base directory is specified, the default remote base directory as specified in %s is used.' % config.get_config_file(),
   'mem':
     'Amount of memory required by this job. Has to be postfixed with one of the following units: M,G, ' +
     'indicating megabytes, gigabytes',
@@ -44,24 +51,44 @@ h = {
     'Loglevel. Default: %s' % util.DEFAULT_LOG_LEVEL.lower(),
 }
 
-def cleanup():
-  ''' close ssh connection. '''
-  if ssh_conn:
-    try:
-      ssh.close_connection(ssh_conn)
-    except:
-      pass
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('-a','--account', help=h['account'], required=False, type=str)
+parser.add_argument('-c','--cmd', help=h['cmd'], required=True, type=str, action='append')
+parser.add_argument('-d','--remotebasedir', help=h['remotebasedir'], required=False, type=str)
+parser.add_argument('-f','--localjobdirfile', help=h['localjobdirfile'], required=True, type=str)
+parser.add_argument('-j','--jobtype', help=h['jobtype'], required=True, type=str)
+parser.add_argument('-l','--logfile', help=h['logfile'], required=False, type=str)
+parser.add_argument('-ll','--loglevel', help=h['loglevel'], required=False, type=str, choices=['debug','info','warn','error','critical'])
+parser.add_argument('-m','--mem', help=h['mem'], required=True, type=str)
+parser.add_argument('-v','--vmem', help=h['vmem'], required=False, type=str)
+parser.add_argument('-w','--walltime', help=h['walltime'], required=True, type=str)
+args = parser.parse_args()
 
-@Retry(max_attempts=5, mm=(0.5,5))
+if args.logfile or args.loglevel:
+  util.setup_logging(args.logfile, args.loglevel)
+log = util.get_log()
+
+# name of the file that contains the list of input files to be uploaded for a job
+infiles_file = 'gridfiles_in.txt'
+ssh_conn = None
+
+# read central configuration file
+try:
+  conf = config.get_config()
+  cluster = conf['CLUSTER']
+  retry = conf['RETRY']
+except:
+  log.critical('failed to read config file %s' % config.get_config_file())
+  log.critical(traceback.format_exc())
+  cleanup()
+  sys.exit(1)
+  
+@Retry(retry['max_attempts'], retry['min_wait_s'], retry['max_wait_s'])
 def get_local_job_directories(localjobdirfile):
   ''' get the list of local job directories from file. '''
-  localdirs = util.read_lines_from_file(localjobdirfile)
-  for localdir in localdirs:
-    if not os.path.isdir(localdir):
-      raise Exception('Local job directory does not exist: %s' % localdir)
-  return localdirs
+  return util.get_local_job_directories(localjobdirfile)
 
-@Retry(max_attempts=5, mm=(0.5,5))
+@Retry(retry['max_attempts'], retry['min_wait_s'], retry['max_wait_s'])
 def prepare_job(ssh_conn, args):
   ''' create remote job directory and job description file. '''
   log.debug('Creating job directory...')
@@ -70,25 +97,26 @@ def prepare_job(ssh_conn, args):
   log.debug('Remote job directory: %s' % remote_jobdir)
   return (remote_jobdir, remote_job_desc_file)
 
-@Retry(max_attempts=5, mm=(0.5,5))
+@Retry(retry['max_attempts'], retry['min_wait_s'], retry['max_wait_s'])
 def stage_in_file(localfile, remotefile):
   ''' upload individual input file. '''
   log.debug('Uploading local file %s to remote file %s' % (localfile, remotefile))
   sftp.put(localfile, remotefile)
 
-@Retry(max_attempts=5, mm=(0.5,5))
+@Retry(retry['max_attempts'], retry['min_wait_s'], retry['max_wait_s'])
 def get_inputfile_names(localdir):
   ''' get the names of the local files to be uploaded prior to starting the job. '''  
   files_in = '%s%s%s' % (localdir,os.path.sep,infiles_file)
-  filenamestmp = util.read_lines_from_file(files_in)
   filenames = []
-  for name in filenamestmp:
-    if not os.path.isabs(name):
-      name = '%s%s%s' % (localdir, os.path.sep, name)
-      filenames.append(name)
+  if os.path.isfile(files_in):
+    filenamestmp = util.read_lines_from_file(files_in)
+    for name in filenamestmp:
+      if not os.path.isabs(name):
+        name = '%s%s%s' % (localdir, os.path.sep, name)
+        filenames.append(name)
   return filenames
   
-@Retry(max_attempts=5, mm=(0.5,5))
+@Retry(retry['max_attempts'], retry['min_wait_s'], retry['max_wait_s'])
 def submit_job(ssh_conn, remote_job_desc_file):
   ''' submit a job. '''
   log.debug('Submitting job...')
@@ -97,68 +125,39 @@ def submit_job(ssh_conn, remote_job_desc_file):
   log.debug('Job ID: %s' % jobid)
   return jobid
 
-@Retry(max_attempts=5, mm=(0.5,5))
-def write_config_file(localdir, jobid, remotedir):
+@Retry(retry['max_attempts'], retry['min_wait_s'], retry['max_wait_s'])
+def create_or_update_job_config_file(localdir, props_dict):
   ''' create metadata file for job in local job directory (ini-format) '''
-  configfile = '%s%s.job.ini' % (localdir, os.path.sep)
-  log.debug('Writing job config file into %s' % configfile)
-  config = ConfigParser.RawConfigParser()
-  config.add_section('JOB')
-  config.set('JOB', 'id', jobid)
-  config.set('JOB', 'remote_job_directory', remotedir)
-  if os.path.exists(configfile):
-    os.remove(configfile)
-  if os.path.exists(configfile):
-    raise Exception('Failed to delete configuration file %s' % configfile)
-  with open(configfile, 'wb') as f:
-    config.write(f)
-  if not os.path.isfile(configfile):
-    raise Exception("Creation of job config file %s failed." % configfile)
+  config.create_or_update_job_config_file(localdir, props_dict)
 
 def stage_in(sftp, localdir, remotedir):
   ''' upload all input files, if any. '''
   localfiles = get_inputfile_names(localdir)
+  log.debug('files to upload: %s' % str(localfiles))
   for localfile in localfiles:
     remotefile = '%s/%s' % (remotedir, os.path.basename(localfile))
     stage_in_file(localfile, remotefile)
-
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('-a','--account', help=h['account'], required=False, type=str)
-parser.add_argument('-c','--cmd', help=h['cmd'], required=True, type=str, action='append')
-parser.add_argument('-d','--remotebasedir', help=h['remotebasedir'], required=True, type=str)
-parser.add_argument('-f','--localjobdirfile', help=h['localjobdirfile'], required=True, type=str)
-parser.add_argument('-j','--jobtype', help=h['jobtype'], required=True, type=str)
-parser.add_argument('-l','--logfile', help=h['logfile'], required=False, type=str)
-parser.add_argument('-ll','--loglevel', help=h['loglevel'], required=False, type=str, choices=['debug','info','warn','error','critical'])
-parser.add_argument('-m','--mem', help=h['mem'], required=True, type=str)
-parser.add_argument('-v','--vmem', help=h['vmem'], required=False, type=str)
-parser.add_argument('-w','--walltime', help=h['walltime'], required=True, type=str)
-
-args = parser.parse_args()
-args.vmem = args.mem if not args.vmem else args.vmem
-if args.logfile or args.loglevel:
-  util.setup_logging(args.logfile, args.loglevel)
-log = util.get_log()
 
 # read local job directories from file
 try:
   localdirs = get_local_job_directories(args.localjobdirfile)
 except:
   log.critical('Failed to read list of local job directories')
+  log.critical(traceback.format_exc())
   cleanup()
   sys.exit(1)
     
 # set up SSH connection
 try:
-  parser = util.get_config_parser()
-  host = parser.get('MAIN', 'remote_host')
-  user = parser.get('MAIN', 'remote_user')
-  privkey = parser.get('MAIN', 'ssh_priv_key_file')
-  ssh_conn = ssh.open_connection_ssh_agent(host, user, privkey)
+  ssh_conn = ssh.open_connection_ssh_agent(cluster['remote_host'], cluster['remote_user'], cluster['ssh_priv_key_file'])
 except:
   log.critical('Failed to set up SSH connection')
   cleanup()
   sys.exit(1)
+
+args.vmem = args.mem if not args.vmem else args.vmem
+args.account = cluster['default_account'] if not args.account else args.account
+args.remotebasedir = cluster['default_remote_base_directory'] if not args.remotebasedir else args.remotebasedir
 
 sftp = ssh_conn.open_sftp()
 
@@ -168,9 +167,10 @@ for localdir in localdirs:
     log.info('Submitting job from %s' % localdir)
     args.jobname = os.path.basename(localdir)
     remote_jobdir, remote_job_desc_file = prepare_job(ssh_conn, args)
+    create_or_update_job_config_file(localdir, { 'JOB': { 'remote_directory': remote_jobdir, 'download_done': False } })
     stage_in(sftp, localdir, remote_jobdir)
     jobid = submit_job(ssh_conn, remote_job_desc_file)
-    write_config_file(localdir, jobid, remote_jobdir)
+    create_or_update_job_config_file(localdir, { 'JOB': { 'id': jobid } })
   except:
     log.error('Problem submitting job in local directory %s. skipping job.' % localdir)
 
