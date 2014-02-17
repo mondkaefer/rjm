d35029f966f1f80d0ce02e84bfb170ea6a624e71
Martin Feller <martinfeller73@gmail.com>
2014-02-17 13:23:09 +1300

    get configuration as dict

--- client/bin/run_remote.py
+++ client/bin/run_remote.py
@@ -1,15 +1,12 @@
 import os
 import sys
-import cer.client.util as util
+import cer.client.util.config as config
 import cer.client.ssh as ssh
 
-parser = util.get_config_parser()
+conf = config.get_config()
+cluster = conf['CLUSTER']
 
-host = parser.get('MAIN', 'remote_host')
-user = parser.get('MAIN', 'remote_user')
-privkey = parser.get('MAIN', 'ssh_priv_key_file')
-
-ssh_conn = ssh.open_connection_ssh_agent(host, user, privkey)
+ssh_conn = ssh.open_connection_ssh_agent(cluster['remote_host'], cluster['remote_user'], cluster['ssh_priv_key_file'])
 cmd = ' '.join(sys.argv[1:])
 rc, stdout, stderr = ssh.run(cmd, ssh_conn)
 ssh.close_connection(ssh_conn)
