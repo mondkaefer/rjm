cf0d53abb86125ed4e771d8c57b93da3dcc1d661
Martin Feller <martinfeller73@gmail.com>
2014-02-17 13:36:44 +1300

    use config dict

--- client/bin/rjm_autenticate.py
+++ client/bin/rjm_autenticate.py
@@ -1,29 +1,26 @@
 import sys
 import traceback
 import cer.client.util as util
+import cer.client.util.config as config
 import cer.client.ssh as ssh
 from paramiko import Agent
 
-fingerprint = None
-parser = None
-
 try:
-  parser = util.get_config_parser()
-  fingerprint = parser.get('MAIN', 'ssh_fingerprint')
+  conf = config.get_config()
 except:
   print >> sys.stderr, "Error: Failed to load configuration file"
   print >> sys.stderr, traceback.format_exc()
   sys.exit(1)
 
 if util.platform_is_windows():
-  ssh.add_private_key_to_agent(util.get_priv_ssh_key())
+  ssh.add_private_key_to_agent(config.get_priv_ssh_key())
   print 'Done'
 else:
   found = False
   agent = Agent()
   agent_keys = agent.get_keys()
   if agent_keys:
-    fingerprint = fingerprint.replace(':','')
+    fingerprint = conf['CLUSTER']['ssh_fingerprint'].replace(':','')
     for key in agent_keys:
       if key.get_fingerprint().encode('hex') == fingerprint:
         found = True
@@ -32,5 +29,5 @@ else:
     print 'SSH key already registered with agent. Nothing to do.'
   else:
     print 'SSH key not registered with agent. Adding...'
-    ssh.add_private_key_to_agent(util.get_priv_ssh_key())
+    ssh.add_private_key_to_agent(config.get_priv_ssh_key())
     print 'Done'
