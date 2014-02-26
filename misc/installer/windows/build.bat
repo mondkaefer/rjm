66f2ace2cc2438c33e209983451a439b363fcb83
Martin Feller <martinfeller73@gmail.com>
2014-02-26 16:49:03 +1300

    load extension

--- misc/installer/windows/build.bat
+++ misc/installer/windows/build.bat
@@ -1,2 +1,2 @@
 candle.exe cer_tools.xml
-light.exe -b exe_path=..\..\..\client\bin\dist -b doc_path=..\..\..\doc cer_tools.wixobj
+light.exe -ext WixUIExtension -b exe_path=..\..\..\client\bin\dist -b doc_path=..\..\..\doc cer_tools.wixobj
