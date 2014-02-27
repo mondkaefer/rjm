0a7c5aee75c7f58566f9ee9406ca38b962900acc
Martin Feller <martinfeller73@gmail.com>
2014-02-28 10:49:00 +1300

    cleaning up

--- misc/samples/windows/small/run.bat
+++
@@ -1,3 +0,0 @@
-rjm_batch_submit.exe -c "bash script.sh" -m 1G -j serial -w 00:01:00 -f localdirs.txt
-rjm_batch_wait.exe -f localdirs.txt -z 5 
-
--- misc/samples/windows/small/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load Octave/3.6.4 
-octave transpose.m
-
