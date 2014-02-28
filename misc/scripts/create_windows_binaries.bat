cfd65f3ac28c22797ee714d4869ed49b7c1f436a
Martin Feller <martinfeller73@gmail.com>
2014-02-28 14:12:07 +1300

    improve comments

--- misc/scripts/create_windows_binaries.bat
+++ misc/scripts/create_windows_binaries.bat
@@ -1,5 +1,7 @@
 REM Create Windows binaries from Python modules
 REM This script must be invoked in the top directory of the git repository
+REM Prerequisites:
+REM  * pyinstaller 
 
 cd client\lib
 python setup.py install
