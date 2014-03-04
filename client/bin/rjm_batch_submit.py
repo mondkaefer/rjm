1dd9daf5660f6224a7f338e7b72f2e817e961319
Martin Feller <martinfeller73@gmail.com>
2014-03-04 16:55:01 +1300

    changing to lower case

--- client/bin/rjm_batch_submit.py
+++ client/bin/rjm_batch_submit.py
@@ -54,7 +54,6 @@ h = {
 }
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('-p','--projectcode', help=h['projectcode'], required=False, type=str)
 parser.add_argument('-c','--cmd', help=h['cmd'], required=True, type=str, action='append')
 parser.add_argument('-d','--remotedir', help=h['remotedir'], required=False, type=str)
 parser.add_argument('-f','--localjobdirfile', help=h['localjobdirfile'], required=True, type=str)
@@ -62,6 +61,7 @@ parser.add_argument('-j','--jobtype', help=h['jobtype'], required=True, type=str
 parser.add_argument('-l','--logfile', help=h['logfile'], required=False, type=str)
 parser.add_argument('-ll','--loglevel', help=h['loglevel'], required=False, type=str, choices=['debug','info','warn','error','critical'])
 parser.add_argument('-m','--mem', help=h['mem'], required=True, type=str)
+parser.add_argument('-p','--projectcode', help=h['projectcode'], required=False, type=str)
 parser.add_argument('-v','--vmem', help=h['vmem'], required=False, type=str)
 parser.add_argument('-w','--walltime', help=h['walltime'], required=True, type=str)
 args = parser.parse_args()
@@ -89,7 +89,7 @@ def get_local_job_directories(localjobdirfile):
 @Retry(conf['RETRY']['max_attempts'], conf['RETRY']['min_wait_s'], conf['RETRY']['max_wait_s'])
 def prepare_job(ssh_conn, args):
   ''' create remote job directory and job description file. '''
-  log.debug('Creating job directory...')
+  log.debug('creating job directory...')
   remote_jobdir, remote_job_desc_file = job.prepare_job(ssh_conn, args.remotedir, args.jobname, args.cmd,
     args.mem, args.vmem, args.walltime, args.jobtype, args.projectcode)
   log.debug('Remote job directory: %s' % remote_jobdir)
@@ -116,7 +116,7 @@ def get_inputfile_names(uploads_file):
 @Retry(conf['RETRY']['max_attempts'], conf['RETRY']['min_wait_s'], conf['RETRY']['max_wait_s'])
 def submit_job(ssh_conn, remote_job_desc_file):
   ''' submit a job. '''
-  log.debug('Submitting job...')
+  log.debug('submitting job...')
   job_base_id = job.submit_job(ssh_conn, remote_job_desc_file)
   jobid = '%s.0' % job_base_id
   log.debug('Job ID: %s' % jobid)
@@ -139,7 +139,7 @@ def stage_in(sftp, uploads_file, remotedir):
 try:
   localdirs = get_local_job_directories(args.localjobdirfile)
 except:
-  log.critical('Failed to read list of local job directories')
+  log.critical('failed to read list of local job directories')
   log.critical(traceback.format_exc())
   cleanup()
   sys.exit(1)
@@ -148,7 +148,7 @@ except:
 try:
   ssh_conn = ssh.open_connection_ssh_agent(conf['CLUSTER']['remote_host'], conf['CLUSTER']['remote_user'], conf['CLUSTER']['ssh_priv_key_file'])
 except:
-  log.critical('Failed to set up SSH connection')
+  log.critical('failed to set up ssh connection')
   cleanup()
   sys.exit(1)
 
@@ -161,7 +161,7 @@ sftp = ssh_conn.open_sftp()
 # create remote job directories, stage files in, submit jobs
 for localdir in localdirs:
   try:
-    log.info('Submitting job from %s' % localdir)
+    log.info('submitting job from %s' % localdir)
     args.jobname = os.path.basename(localdir)
     remote_jobdir, remote_job_desc_file = prepare_job(ssh_conn, args)
     create_or_update_job_config_file(localdir, { 'JOB': { 'remote_directory': remote_jobdir, 'download_done': False } })
@@ -170,6 +170,6 @@ for localdir in localdirs:
     jobid = submit_job(ssh_conn, remote_job_desc_file)
     create_or_update_job_config_file(localdir, { 'JOB': { 'id': jobid } })
   except:
-    log.error('Problem submitting job in local directory %s. skipping job.' % localdir)
+    log.error('problem submitting job in local directory %s. skipping job.' % localdir)
 
 cleanup()
