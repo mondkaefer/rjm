f88f22dde18af8a273fd50d4847300c977411fdd
Martin Feller <martinfeller73@gmail.com>
2014-03-05 15:44:34 +1300

    remove postfix

--- misc/samples/windows/run.bat
+++ misc/samples/windows/run.bat
@@ -1,3 +1,3 @@
-rjm_batch_submit.exe -c "bash script.sh" -m 1G -j serial -w 00:01:00 -f localdirs.txt
-rjm_batch_wait.exe -f localdirs.txt -z 5 
+rjm_batch_submit -c "bash script.sh" -m 1G -j serial -w 00:01:00 -f localdirs.txt
+rjm_batch_wait -f localdirs.txt -z 5 
 
