eee4eab844df67fcefa3afbfe866afd472ff930b
Martin Feller <martinfeller73@gmail.com>
2014-03-06 23:18:14 +1300

    clean up at the end

--- misc/samples/mac/run.sh
+++ misc/samples/mac/run.sh
@@ -1,3 +1,4 @@
 python rjm_batch_submit.py -c "bash script.sh" -m 1G -j serial -w 00:05:00 -f localdirs.txt
 python rjm_batch_wait.py -f localdirs.txt -z 5
+python rjm_batch_clean.py -f localdirs.txt
 
